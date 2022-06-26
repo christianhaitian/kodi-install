1eeab2a6dee2dcea7be94b6b75fd8ba238acade7
christianhaitian <christianhaitian0@gmail.com>
2022-06-26 00:10:45 +0000

    Add some patches and update build to include avi playing support

---
+++ build-binary-addons-arkos.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Builds Kodi binary addons except PVR addons.
+# Uses Kodi standard repository for binary addons.
+
+# Configure Kodi standard repository for binary addons.
+repofname="/home/kodi/kodi-source/cmake/addons/bootstrap/repositories/binary-addons.txt"
+bin_addons_repo="binary-addons https://github.com/xbmc/repo-binary-addons.git master"
+rm -f $repofname
+# -n no trailing newline
+echo -n $bin_addons_repo >> $repofname
+
+# Build the addons
+num_proc=`nproc`
+echo "Using $num_proc processors"
+cd /home/kodi/kodi-source
+
+rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="audiodecoder.*"
+
+rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="audioencoder.*"
+
+#rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+#make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="game.*"
+
+#rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+#make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="imagedecoder.*"
+
+rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="inputstream.*"
+
+rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="peripheral.*"
+
+rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="pvr.*"
+
+#rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+#make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="screensaver.*"
+
+rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="vfs.*"
+
+#rm -f /home/kodi/kodi-source/tools/depends/target/binary-addons/.installed-native
+#make -j$num_proc -C tools/depends/target/binary-addons PREFIX=/home/kodi/bin-kodi ADDONS="visualization.*"
--- configure-kodi-ubuntu-bionic.sh
+++ configure-kodi-ubuntu-bionic.sh
@@ -5,10 +5,15 @@
 libmali/libmali.sh
 mkdir -p /home/kodi/kodi-build
 cd ../kodi-source
-patch -Np1 < ../kodi-install/patches/*.patch
+
+for i in /home/kodi/kodi-install/patches/*.patch
+do 
+    patch -Np1 < $i
+done
+
 cd /home/kodi/kodi-build
 
 # Options: -DVERBOSE=ON
 
-cmake ../kodi-source -DCMAKE_INSTALL_PREFIX=/home/kodi/bin-kodi -DENABLE_INTERNAL_FLATBUFFERS=ON -DCORE_PLATFORM_NAME=gbm -DAPP_RENDER_SYSTEM=gles
+cmake ../kodi-source -DCMAKE_INSTALL_PREFIX=/home/kodi/bin-kodi -DENABLE_INTERNAL_FLATBUFFERS=ON -DENABLE_INTERNAL_DAV1D=ON -DCORE_PLATFORM_NAME=gbm -DAPP_RENDER_SYSTEM=gles
 
