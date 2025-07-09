242d414ab352e9c2987b7f3f777fb055447e6107
christianhaitian <christianhaitian0@gmail.com>
2025-07-09 22:57:59 +0000

    Add missing configuration.sh file

---
+++ configuration.sh
@@ -0,0 +1,10 @@
+# Kodi install configuration file.
+# No trailing / in these variables.
+
+# Kodi source directory.
+KODI_SOURCE_DIR=/home/kodi/kodi-source
+BRANCH=Omega
+# Kodi stable version using Python 3.
+KODI_SOURCE_TAG=21.2-Omega
+KODI_BUILD_DIR=/home/kodi/kodi-build
+KODI_INSTALL_DIR=/home/kodi/kodi-bin
